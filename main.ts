0eb7946dca744293b1838262a14a77626eeec948
Talha Ameer <talhaameer713@gmail.com>
2024-03-30 12:46:49 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,63 @@
+// let isHungry = false;
+// let snack = isHungry ? "apple" : "water" ;
+// console.log(`You should have some ${snack}.`)
+
+
+// function greet() {
+//     return "Hello World";
+// }
+// let message = greet();
+// console.log(message);
+
+
+// function greet() {
+//     console.log("Hello World");
+// }
+// greet()
+
+
+// function abc() {
+//     let owais = 1 + 1.5 +2;
+//     console.log(owais)
+// }
+// abc()
+
+
+// function sum() {
+//     2 + 2;
+// }
+// let result = sum();
+// console.log(result); // undefined means empty
+
+
+// function sum() {
+//     return 4 / 2;
+// }
+// let result = sum();
+// console.log(result); // 2
+
+                    // parameters
+// function sum(num1: number, num2: number) {
+//     let answer =  num1 + num2;
+//     return answer;
+//     let a = 2; // return ke baad jo bhi likho ge nahi chalega
+// }
+//                 //arguments
+// let result = sum(5, 9);
+// console.log(result);
+
+
+// function greet(message: string){
+//     return message;
+// }
+// let result = greet("hello hamza");
+// console.log(result);
+
+
+// DEFAULT PARAMETERS
+
+function greet(message = "hello world") {
+    return message;
+}
+let result = greet("hello talha");
+console.log(result);
